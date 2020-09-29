82fa99c3c4134153124b32a36600baa4f18fe5f6
vistritpandey <vistritpandey@gmail.com>
2020-09-29 17:32:22 +0530

    fixed js

--- script.js
+++ script.js
@@ -24,17 +24,13 @@ function addTodo(todo) {
             todoEl.classList.add("completed");
         }
         todoEl.innerText = todoText;
-
         todoEl.addEventListener("click", () => {
             todoEl.classList.toggle("completed");
-
             updateLS();
         });
         todoEl.addEventListener("contextmenu", (e) => {
             e.preventDefault();
-
             todoEl.remove();
-
             updateLS();
         });
         todosUL.appendChild(todoEl);
